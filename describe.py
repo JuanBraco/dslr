d15c432b64549a9ec136c2f566ef0a5f6cd2c7a8
Juan Braco <jbraco92@gmail.com>
2023-11-27 17:12:16 +0100

    data treatment

---
+++ describe.py
@@ -0,0 +1,29 @@
+from load_csv import load
+import ft_stat
+import pandas as pd
+
+
+def main():
+    dataset = load("data/dataset_train.csv")
+    numerical_dataset = dataset.select_dtypes(include=['int', 'float'])
+
+    d = {}
+    for column in numerical_dataset:
+        if column == "Index" or column == "Best Hand":
+            continue
+        args = ft_stat.clean_list(list(numerical_dataset[column]))
+        count = len(args)
+        mean = ft_stat.ft_mean(args)
+        std = (ft_stat.ft_var(args)) ** 0.5
+        min, max = ft_stat.ft_min_max(args)
+        first_q, third_q = ft_stat.ft_quartile(args)
+        median = ft_stat.ft_median(args)
+        d[column] = [count, mean, std, min, first_q, median, third_q, max]
+    index = ["count", "mean", "std", "min", "25%", "50%", "75%", "max"]
+    df = pd.DataFrame(d, index)
+    print(df)
+    print(numerical_dataset.describe())
+
+
+if __name__ == "__main__":
+    main()
---
+++ ft_stat.py
@@ -0,0 +1,40 @@
+def clean_list(args):
+    return [x for x in args if x == x]
+
+
+def ft_min_max(args):
+    args.sort()
+    return args[0], args[-1]
+
+
+def ft_mean(args):
+    """calculating mean"""
+    return sum(args) / len(args)
+
+
+def ft_median(args):
+    """calculating median"""
+    sorted_args = sorted(args)
+    n = len(sorted_args)
+    if n % 2 == 1:
+        return sorted_args[n // 2]
+    else:
+        return (sorted_args[n // 2 - 1] + sorted_args[n // 2]) / 2
+
+
+def ft_quartile(args):
+    """calculating first and third quartile using linear interpolation"""
+    sorted_args = sorted(args)
+    n = len(sorted_args)
+    q1_idx = (n - 1) // 4
+    q1_f = (n - 1) % 4 / 4
+    q1 = (1 - q1_f) * sorted_args[q1_idx] + q1_f * sorted_args[q1_idx + 1]
+    q3_idx = 3 * (n - 1) // 4
+    q3_f = (3 * (n - 1)) % 4 / 4
+    q3 = (1 - q3_f) * sorted_args[q3_idx] + q3_f * sorted_args[q3_idx + 1]
+    return [q1, q3]
+
+
+def ft_var(args):
+    mean = ft_mean(args)
+    return sum((x - mean) ** 2 for x in args) / (len(args) - 1)
